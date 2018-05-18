192c78b8d4ef157eb7dc648a38ed5ecff19e5374
Hengli Yin <yinhengliben@gmail.com>
2018-05-18 21:20:40 +0800

    fix demo

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,29 @@
-var height;
-var width;
-
-
-
-
 $(function(){
   var height = document.documentElement.clientHeight;
   var width = document.documentElement.clientWidth;
   changeOrientation($('.body-container'));
   changeOrientation($('.swiper-slide-content'));
-  
 });
-
 
 function showTip(){
   $('.tip').removeClass('tip').addClass('animated fadeIn');
+}
+
+function gotoPart2(){
+  $('.part1').hide();
+  $('.part2').show();
+  var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    freeMode: true,
+    on: {
+      touchMove(e) {
+        // console.log(e.touches[0].screenY);
+      },
+      slideNextTransitionStart(e) {
+        console.log(this.activeIndex);
+      },
+    }
+  });
 }
 
